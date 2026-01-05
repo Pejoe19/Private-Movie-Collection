db148738fee0229a74f690181b13499f4b8a5420
Pernille Jørgensen <pernilleajorgensen@gmail.com>
2026-01-05 10:26:02 +0100

    Project created

---
+++ src/main/java/dk/easv/privatemoviecollection/Launcher.java
@@ -0,0 +1,7 @@
+package dk.easv.privatemoviecollection;
+
+public class Launcher {
+    public static void main(String[] args) {
+        javafx.application.Application.launch(Application.class, args);
+    }
+}
